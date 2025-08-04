ed92f87bac8a5acc8224fbc57760302ca597de25
Junao BH <junaocrypto@gmail.com.com>
2025-08-04 18:22:44 +0100

    T

--- infra/main.tf
+++ infra/main.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = ">= 1.12.0"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.38.1"
-    }
-  }
-
 }
 
 provider "azurerm" {
